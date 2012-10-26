2549d6311457c63b28d657c0843cf0b9b56aa770
Bryan Stedman <bryan@bryanstedman.com>
2012-10-26 09:20:42 -0700

    removed unnecessary files

--- characterMapping.jquery.js
+++ characterMapping.jquery.js
@@ -1,19 +1,26 @@
-(function($, window, document, undefined){
-  var Map = {
-    init: function() {
-      
+;(function($){
+  $.fn.mapChars = function(from, to, reverse) {
+    var self = this;
+    var start = from;
+    var finish = to;
+    var type = 'text';
+    var toTranslate = self.text();
+    var newContent = "";
+    if (reverse) {
+        start = to;
+        finish = from;
     }
 
+    for (i=0;i<toTranslate.length;i++) {
+        for (j=0;j<start.length;j++) {
+            if (toTranslate[i] === ' ') {
+                newContent = newContent + ' ';
+            } else if (toTranslate[i] === start[j]) {
+                newContent = newContent + finish[j];
+            }
+        }
+    }
+    console.log(newContent);
+    self.text(newContent);
   };
-
-  $.fn.mapChars = function() {
-    return this.each(function() {
-
-    });
-  };
-
-  $.fn.mapChars.options = {
-
-  };
-
-})(jQuery, window, document);
+})(jQuery);
